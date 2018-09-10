1e54f4b0dcc7efe83a2cb761009acb5d6d36a8dc
Peter-32 <peter@impactradius.com>
2018-09-10 11:51:57 -0700

    code_consolidated

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ _
 
 - **Definition of the problem**:
 Forecast one day ahead for thousands/millions of wikipedia pages
-- **Goal**: An improvement of 50% the difference between the baseline score and the best Kaggle score.  Deadline: Monday, September 17
+- **Goal**: Univariate time series forecasting on the first wikipedia page from on the train_1.csv file.  Deadline: Monday, September 17
 - **Initial Pipeline**: Loading the train_1.csv file from the [Kaggle Competition](https://www.kaggle.com/c/web-traffic-time-series-forecasting)
 - **Purpose**: The purpose of this project is to improve my ability to forecast
 - **Example Business Use Cases** for forecasting are:
@@ -32,7 +32,13 @@ Forecast one day ahead for thousands/millions of wikipedia pages
 12. Keep track of key performance indicators of success for your business and competition to identify threats and change strategies (i.e. budget for improving the product in the short term, sales/marketing, hiring, or research and development) (Keep in mind how you stand out among the competition and how that has changed)
 13. Keeping track of number of customer support data over time to find stories that help you change your tactics to address problem (i.e. time to resolution, time to first response, hiring to address high velocity of tickets, transferring employees to other teams if there is low velocity of tickets, number of high priority tickets)
 14. Determining the need to open/close new warehouses
-15. Determining when suppliers are charging or providing too little value
+15. Determining when suppliers are trending towards are charging too much, or providing too little value.  Which times are the prices the lowest?
 16. Finding the effect of different marketing efforts on closing sales
 
 - There are hundreds of ways to add value to a business with forecasting.  Companies need to stay competitive and stand out among the competition to survive.  Use forecasting to keep an edge by adding value in one of the ways mentioned above.  As mentioned above, prevention of threatening events alone can save a company millions.  Fund a forecasting project today.
+
+### Project Ideas
+
+There are basically three often used approaches to make time series stable based on three difference scenarios: 1) first difference for linear trend; 2) log for non-linear trend; 3) log seasonal difference for seasonality.
+
+Other methods are square, log difference, lag. However, sometime it is difficult to get stationary, then you could try different combinations of those techniques for example log square difference and so on. Stationarity is tested via unit root for example ADF, ADF-GLS, KPSS, PP, CH. If you can reject the null of unit root, you can confirm that the time series is stationary. There are three different models from ADF test: 1) no constant (not always used and be very cautious with this one); 2) with constant; 3) with constant and trend. You have to model ARIMA accordingly based on the test for example you must include a constant in ARIMA if you use the second and both a constant and a trend for the third. Please note that the second ADF model is commonly used, however wrong tests could reduce the power of ADF. Therefore, I also recommend you run several comparable tests to get a more robust result. According to Hacker (2010), the Schwarz information criterion can be helpful in determine which testing models to use in ADF.
--- main.py
+++ main.py
@@ -1,8 +1,80 @@
 # Imports
+# import seaborn as sns
+import numpy as np
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+# import missingno as msno
+# from sklearn.preprocessing import StandardScaler
+from pandasql import sqldf
+from pandas.plotting import autocorrelation_plot
+from statsmodels.tsa import stattools
+q = lambda q: sqldf(q, globals())
+# pd.set_option('display.mpl_style', 'default')
+# pd.set_option('display.line_width', 5000)
+# pd.set_option('display.max_columns', 60)
+mpl.rcParams['figure.figsize'] = (15.0, 5.0)
 
-# Read in data
-df = pd.read_csv('data/train_1.csv', sep=",", nrows=1)
-df = df.T
-df.columns = df.iloc[0]
-df = df.iloc[1:]
+# Prepare data
+
+def transpose_and_set_column_names(page_visits):
+    page_visits = page_visits.T
+    page_visits.columns = page_visits.iloc[0]
+    return page_visits.iloc[1:]
+
+def set_index_datatype_to_datetime(page_visits):
+    page_visits['_Date_'] = pd.to_datetime(page_visits.index)
+    page_visits.set_index(['_Date_'])
+    return page_visits.drop(['_Date_'], axis=1)
+
+def convert_all_columns_to_integers(page_visits):
+    for col in page_visits.columns:
+        page_visits[col] = page_visits[col].astype(str).astype(int)
+    return page_visits
+
+page_visits = pd.read_csv('data/train_1.csv', sep=",", nrows=1)
+page_visits = transpose_and_set_column_names(page_visits)
+page_visits = set_index_datatype_to_datetime(page_visits)
+page_visits = convert_all_columns_to_integers(page_visits)
+
+# Stationary Process
+
+def get_first_order_differencing(page_visits):
+    first_order_diff = page_visits.diff(1)
+    return first_order_diff.iloc[1:]
+
+def plot_page_visits_and_first_order_differencing(page_visits, first_order_diff):
+    fig, ax = plt.subplots(2, sharex=True)
+    page_visits.plot(ax=ax[0], color='b')
+    ax[0].set_title("Page visits")
+    first_order_diff.plot(ax=ax[1], color='r')
+    ax[1].set_title("First-order differences to page visits")
+    plt.savefig('plots/1_page_visits.png')
+    print("1_page_visits plot created\n")
+
+def plot_ACF_page_visits_and_first_order_differencing(page_visits, first_order_diff):
+    fig, ax = plt.subplots(2, sharex=True)
+    autocorrelation_plot(page_visits, ax=ax[0], color='b')
+    ax[0].set_title("ACF - Page visits")
+    autocorrelation_plot(first_order_diff.iloc[1:], ax=ax[1], color='r')
+    ax[1].set_title("ACF - First-order differences to page visits")
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=2.0)
+    plt.savefig('plots/2_acf.png')
+    print("2_acf plot created\n")
+
+def print_null_hypothesis__is_stationary(time_series, time_series_name):
+    print("Testing {}, the null hypothesis is that the series is stationary.  If all are accepted, let's just say the data is essentially random.".format(time_series_name))
+    alpha = 0.05
+    acf_time_series, confint_time_series, qstat_time_series, pvalues_time_series = stattools.acf(time_series,unbiased=True,nlags=20,qstat=True,alpha=alpha)
+    for l, p_val in enumerate(pvalues_time_series):
+        if p_val > alpha:
+            print("Null hypothesis is accepted at lag = {} for p-val = {}".format(l, p_val))
+        else:
+            print("Null hypothesis is rejected at lag = {} for p-val = {}".format(l, p_val))
+    print()
+
+first_order_diff = get_first_order_differencing(page_visits)
+plot_page_visits_and_first_order_differencing(page_visits, first_order_diff)
+plot_ACF_page_visits_and_first_order_differencing(page_visits, first_order_diff)
+print_null_hypothesis__is_stationary(page_visits, "page_visits")
+print_null_hypothesis__is_stationary(first_order_diff, "first_order_diff")
